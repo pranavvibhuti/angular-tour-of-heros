8d2cd951cea32129e98c3c75a235f228f33feded
Pranav Jha <pranjha@deloitte.com>
2019-10-25 03:05:51 +0530

    after chapter 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,8 +13,13 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   }
-  
+
   heroes = HEROES;
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
